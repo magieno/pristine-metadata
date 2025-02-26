bca10a15e315c2357db695970692b323ba836d7f
Etienne Noel <etiennenoel@gmail.com>
2025-02-26 08:53:05 -0800

    - Update

--- src/decorators/decorators.ts
+++ src/decorators/decorators.ts
@@ -1,3 +1,6 @@
 export * from "./classMetadata.decorator";
+export * from "./clone-class-metadata.decorator";
+export * from "./clone-method-metadata.decorator";
+export * from "./clone-property-metadata.decorator";
 export * from "./method.decorator";
 export * from "./property.decorator";
